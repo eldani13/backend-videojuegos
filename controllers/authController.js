eb8ce0220491e5b5ff04e0d11bb617a703e29cb1
eldani13 <ll-luis123@hotmail.com>
2024-10-14 12:39:59 -0500

    Uploads x7

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,17 +23,41 @@ const registerUser = async (req, res) => {
   }
 };
 
+// const loginUser = async (req, res) => {
+//   const { username, password } = req.body;
+
+//   const user = await User.findOne({ username });
+//   if (!user) return res.status(400).json({ message: 'Credenciales inválidas' });
+
+//   const isMatch = await bcrypt.compare(password, user.password);
+//   if (!isMatch) return res.status(400).json({ message: 'Credenciales inválidas' });
+
+//   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });  
+//   res.json({ token });
+// };
+
+
 const loginUser = async (req, res) => {
   const { username, password } = req.body;
 
-  const user = await User.findOne({ username });
-  if (!user) return res.status(400).json({ message: 'Credenciales inválidas' });
+  try {
+    const user = await User.findOne({ username });
+    if (!user) return res.status(400).json({ message: 'Credenciales inválidas' });
 
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) return res.status(400).json({ message: 'Credenciales inválidas' });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: 'Credenciales inválidas' });
 
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-  res.json({ token });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    
+    if (!token) {
+      return res.status(500).json({ message: "Error al generar el token" });
+    }
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ message: 'Error del servidor' });
+  }
 };
+
 
 module.exports = { registerUser, loginUser };
