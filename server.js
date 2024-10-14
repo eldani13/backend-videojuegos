86873592f2fec02f4f117c9ef7d3521ebf22c825
eldani13 <ll-luis123@hotmail.com>
2024-10-14 11:17:37 -0500

    Uploads x6

--- server.js
+++ server.js
@@ -43,11 +43,7 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors({
-  origin: "https://backend-videojuegos.onrender.com",
-  methods: "GET,POST,PUT,DELETE",
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 app.use('/api', productRoutes);
